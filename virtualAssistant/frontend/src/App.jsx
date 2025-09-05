5f0d59ec91e198def1842cb504e45e5b14c1baed
isikaghosh28 <isikaghosh90@gmail.com>
2025-09-05 21:55:13 +0530

    Update App.jsx

--- virtualAssistant/frontend/src/App.jsx
+++ virtualAssistant/frontend/src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function App(){
   return(
-    <div>
-
-    </div>
+     <Routes>
+       <Route path='/signup' element={<SignUp/>}/>
+       <Route path='/signin' element={<SignIn/>}/>
+     </Routes>
   )
 }
 
